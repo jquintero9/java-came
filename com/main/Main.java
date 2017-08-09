ac6f49380252bf67ce686cb8ec4ea6f8d7c4136b
jhon jaime quintero correa <jhjaquintero@gmail.com>
2017-08-09 16:33:17 -0500

    Se modificó el archivo Main.java

--- com/main/Main.java
+++ com/main/Main.java
@@ -8,8 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Game");
 		window.setContentPane(new Game());
-		window.pack();
 		window.setResizable(false);
+		window.pack();
 		window.setLocationRelativeTo(null);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
